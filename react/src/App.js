4a9800882643a223f9bbc07dd9c27921aed3aea5
Ryan <ryan@apples-MacBook-Pro-3.local>
2022-06-14 00:57:36 +0800

    reconfigure react

--- react/src/App.js
+++ react/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 
   async loadData() {
     try {
-      const res = await fetch('http://localhost/stats');
+      const res = await fetch('http://smartcow.local/stats');
       const blocks = await res.json();
       const ram = blocks.ram;
       const cpu = blocks.cpu;
